21fc1514035f758f7a0aced1c9da25b456d71b7b
arloncunha <arloncunha@gmail.com>
2022-05-05 10:15:50 -0300

    Create PuzzleData structure as class

--- 2021/Day 4/aoc2021_04.py
+++ 2021/Day 4/aoc2021_04.py
@@ -2,6 +2,8 @@
 # --- Day 4: Giant Squid ---
 # https://adventofcode.com/2021/day/4
 
+# ------------- DEBUG SESSION --------------
+
 class BackgroundColors:
     HEADER = '\033[95m'
     BLUE = '\033[94m'
@@ -13,8 +15,6 @@ class BackgroundColors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-# Python program to print
-# colored text and background
 def print_red(skk, end=""): print("\033[91m {}\033[00m" .format(skk), end=end)           # noqa:E704
 def print_green(skk, end=""): print("\033[92m {}\033[00m" .format(skk), end=end)         # noqa:E704
 def print_yellow(skk, end=""): print("\033[93m {}\033[00m" .format(skk), end=end)        # noqa:E704
@@ -34,29 +34,13 @@ def debug_board(board_dict: dict):
         return
     board = list(board_dict)
 
-    for y in range(GRID_SIZE):
-        for x in range(GRID_SIZE):
-            if board_dict[board[x + y * GRID_SIZE]] == 'X':
-                print(f"{BackgroundColors.YELLOW}{board[x + y * GRID_SIZE]:<10}{BackgroundColors.RESET}", " ", end="")
+    for y in range(PuzzleData.GRID_SIZE):
+        for x in range(PuzzleData.GRID_SIZE):
+            if board_dict[board[x + y * PuzzleData.GRID_SIZE]] == PuzzleData.NUMBER_MARKED:
+                print(f"{BackgroundColors.YELLOW}{board[x + y * PuzzleData.GRID_SIZE]:<10}{BackgroundColors.RESET}", " ", end="")
             else:
-                print(f"{board[x + y * GRID_SIZE]:<10}", " ", end="")
+                print(f"{board[x + y * PuzzleData.GRID_SIZE]:<10}", " ", end="")
         print()
-
-def has_board_won(board_dict):
-    """Board wins when it has at least one complete row or column of marked numbers
-    """
-    board = list(board_dict)
-
-    for y in range(GRID_SIZE):
-        hits_row = 0
-        hits_col = 0
-        for x in range(GRID_SIZE):
-            hits_row = hits_row + 1 if board_dict[board[x + y * GRID_SIZE]] == 'X' else 0
-            hits_col = hits_col + 1 if board_dict[board[y + x * GRID_SIZE]] == 'X' else 0
-        if hits_row == 5 or hits_col == 5:
-            return True
-    return False
-
 
 def debug_sum_of_all_unmarked_numbers(board_dict):
     """The score of the winning board can now be calculated.
@@ -70,12 +54,44 @@ def debug_sum_of_all_unmarked_numbers(board_dict):
     res: int = 0
     print("debug_sum_of_all_unmarked_numbers: ", end="")
     for key, hit in board_dict.items():
-        res = res + int(key) if hit != 'X' else res
-        if hit == 'X':
+        res = res + int(key) if hit != PuzzleData.NUMBER_MARKED else res
+        if hit == PuzzleData.NUMBER_UNMARKED:
             print_yellow(" + " + str(int(key)))
     print_yellow(" = " + str(res))
     print()
     return res
+
+# ------------- DATA --------------
+
+class PuzzleData:
+    """Data Section, declares global vars used accross the puzzle
+       UPPERCASE_VARIABLE: to be used as constants (not locked!)
+    """
+    GRID_SIZE:        int = 5
+    NUMBER_MARKED:    int = 1
+    NUMBER_UNMARKED:  int = 0
+
+    draw_numbers_list: list = []
+    board_dict_list:   list = []
+
+# ------------- DECLARATIONS --------------
+
+
+def has_board_won(board_dict):
+    """Board wins when it has at least one complete row or column of marked numbers
+    """
+    board = list(board_dict)
+
+    for y in range(PuzzleData.GRID_SIZE):
+        hits_row = 0
+        hits_col = 0
+        for x in range(PuzzleData.GRID_SIZE):
+            hits_row = (hits_row + board_dict[board[x + y * PuzzleData.GRID_SIZE]])
+            hits_col = (hits_col + board_dict[board[y + x * PuzzleData.GRID_SIZE]])
+        if hits_row == 5 or hits_col == 5:
+            return True
+    return False
+
 
 def sum_of_all_unmarked_numbers(board_dict):
     """The score of the winning board can now be calculated.
@@ -84,56 +100,79 @@ def sum_of_all_unmarked_numbers(board_dict):
     """
     res: int = 0
     for key, hit in board_dict.items():
-        res = res + int(key) if hit != 'X' else res
+        res = res + int(key) if hit != PuzzleData.NUMBER_MARKED else res
     return res
 
-
-# main()
-if __name__ == '__main__':
+def load_input_file():
     file = "input.txt" if not __debug__ else 'test.txt'
     with open(file, 'r') as reader:
         input_file = [i.rstrip("\n") for i in reader.readlines()]
+    if len(input_file) == 0:
+        assert False
+    return input_file
 
-    # Data section
-    GRID_SIZE: int = 5
-    draw_numbers_list: list = input_file[0].split(",")
-    board_dict_list: list = []
+# ------------- MAIN METHOD --------------
+
+def main():
+
+    input_file = load_input_file()
+    debug_print(input_file)
+    print_yellow("load file...", end="\n")
+
+    # init data
+    PuzzleData.draw_numbers_list: list = input_file[0].split(",")
+    PuzzleData.board_dict_list: list = []
+
 
     # loading board: list of dict with list[x, y, hit] = coordinates and hit or not
     b = {}
     for line in input_file[2:]:
         row = line.split()
         if row == []:
-            board_dict_list.append(b)
+            PuzzleData.board_dict_list.append(b)
             b = {}
         else:
             for number in row:
-                b[number] = number
+                b[number] = PuzzleData.NUMBER_UNMARKED
 
-    board_dict_list.append(b)
+    PuzzleData.board_dict_list.append(b)
 
+    debug_print(PuzzleData.draw_numbers_list)
+    debug_print(PuzzleData.board_dict_list)
+    print_yellow("init data...", end="\n")
 
     part1 = 0
     part2 = 0
-    for draw in draw_numbers_list:
-        debug_print("----------------------")
-        debug_print("draw: " + draw)
+    for draw in PuzzleData.draw_numbers_list:
         i = -1
-        while len(board_dict_list) > 0 and i < len(board_dict_list) - 1:
+        debug_print(" draw: " + str(draw))
+        while len(PuzzleData.board_dict_list) > 0 and i < len(PuzzleData.board_dict_list) - 1:
             i += 1
-            board = board_dict_list[i]
+            board = PuzzleData.board_dict_list[i]
             if draw in board:
-                board[draw] = 'X'
+                board[draw] = PuzzleData.NUMBER_MARKED
+                if draw == '13':
+                    debug_print(">>>>>>> draw 13 :-( ")
+                    debug_board(board)
+
                 if has_board_won(board):
+                    debug_print(">>>>>>> draw " + draw + " won!")
                     if part1 == 0:
                         part1 = sum_of_all_unmarked_numbers(board) * int(draw)
-                        debug_print("part1: " + str(part1))
+                    part2 = sum_of_all_unmarked_numbers(board) * int(draw)
+
                     debug_board(board)
                     debug_sum_of_all_unmarked_numbers(board)
-                    part2 = sum_of_all_unmarked_numbers(board) * int(draw)
+                    debug_print("part1: " + str(part1))
                     debug_print("part2: " + str(part2))
-                    del board_dict_list[i]
+
+                    del PuzzleData.board_dict_list[i]
                     i -= 1
 
     print("part1:", part1)
     print("part2:", part2)
+
+# ------------- MAIN --------------
+
+if __name__ == '__main__':
+    main()
